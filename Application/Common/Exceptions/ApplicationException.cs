cdc3f091af903b1927c3573ed8eb8a70fba3b31b
Ece <Ece@DESKTOP>
2021-10-18 02:21:40 +0300

    added exceptions for each layer

--- Application/Common/Exceptions/ApplicationException.cs
+++ Application/Common/Exceptions/ApplicationException.cs
@@ -1,6 +1,17 @@
-﻿namespace MerchandisingManagement.Application.Common.Exceptions
+﻿using System;
+
+namespace MerchandisingManagement.Application.Common.Exceptions
 {
-	class ApplicationException
+	class ApplicationException : Exception
 	{
+		internal ApplicationException(string message)
+			: base(message)
+		{
+		}
+
+		internal ApplicationException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
 	}
 }
---
+++ Infrastructure/Exceptions/InfraStructureException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MerchandisingManagement.Infrastructure.Exceptions
+{
+	class InfrastructureException : Exception
+	{
+		internal InfrastructureException(string message)
+			: base(message)
+		{
+		}
+
+		internal InfrastructureException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+	}
+}
